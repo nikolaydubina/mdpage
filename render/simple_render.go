0e51b1d608ddebed62af9a95bbf1e375fbd3e9cb
Nikolay <nikolay.dubina.pub@gmail.com>
2023-05-24 21:28:55 +0800

    out

--- render/simple_render.go
+++ render/simple_render.go
@@ -1,6 +1,7 @@
 package render
 
 import (
+	"io"
 	"strings"
 	"unicode"
 
@@ -10,76 +11,61 @@ import (
 type AuthorRenderer interface{ Render(s string) string }
 
 type SimplePageRender struct {
-	b              *strings.Builder
 	authorRenderer AuthorRenderer
 }
 
 func NewSimplePageRender(authorRenderer AuthorRenderer) SimplePageRender {
 	return SimplePageRender{
-		b:              &strings.Builder{},
 		authorRenderer: authorRenderer,
 	}
 }
 
-func (s SimplePageRender) estimatePageSize(page page.Page) int {
-	c := 0
-	c += 1000 // header
-	for _, q := range page.Groups {
-		c += len(q.Entries) * 1000
-	}
-	return c
+func (s SimplePageRender) RenderTo(out io.StringWriter, page page.Page) {
+	s.RenderHeader(out, page.Header)
+	out.WriteString("\n")
+	s.RenderPageSummary(out, page)
+	out.WriteString("\n")
+	s.RenderPageContent(out, page)
+	out.WriteString("\n")
 }
 
-func (s SimplePageRender) RenderPage(page page.Page) string {
-	s.b.Reset()
-	s.b.Grow(s.estimatePageSize(page))
+func (s SimplePageRender) RenderHeader(out io.StringWriter, header string) { out.WriteString(header) }
 
-	s.RenderHeader(page.Header)
-	s.b.WriteRune('\n')
-	s.RenderPageSummary(page)
-	s.b.WriteRune('\n')
-	s.RenderPageContent(page)
-	s.b.WriteRune('\n')
-	return s.b.String()
-}
-
-func (s SimplePageRender) RenderHeader(header string) { s.b.WriteString(header) }
-
-func (s SimplePageRender) RenderPageSummary(page page.Page) {
-	s.b.WriteString("## ")
-	s.b.WriteString(page.Contents.Title)
-	s.b.WriteRune('\n')
-	s.b.WriteRune('\n')
+func (s SimplePageRender) RenderPageSummary(out io.StringWriter, page page.Page) {
+	out.WriteString("## ")
+	out.WriteString(page.Contents.Title)
+	out.WriteString("\n")
+	out.WriteString("\n")
 
 	entryPrefix := "   "
 	for _, group := range page.Groups {
-		s.b.WriteString(" - " + group.Title)
-		s.b.WriteRune('\n')
+		out.WriteString(" - " + group.Title)
+		out.WriteString("\n")
 		for _, entry := range group.Entries {
-			s.b.WriteString(entryPrefix + "+ " + s.RenderSummaryEntry(entry, page.EntryConfig))
-			s.b.WriteRune('\n')
+			out.WriteString(entryPrefix + "+ " + RenderSummaryEntry(out, entry, page.EntryConfig))
+			out.WriteString("\n")
 		}
 	}
 }
 
-func (s SimplePageRender) RenderSummaryEntry(entry page.Entry, config page.EntryConfig) string {
+func RenderSummaryEntry(out io.StringWriter, entry page.Entry, config page.EntryConfig) string {
 	return "[" + config.TitlePrefix + " " + entry.Title + "](" + makeMarkdownTitleLink(entry.Title) + ")"
 }
 
-func (s SimplePageRender) RenderPageContent(page page.Page) {
+func (s SimplePageRender) RenderPageContent(out io.StringWriter, page page.Page) {
 	for _, group := range page.Groups {
-		s.RenderGroupContent(group, page.EntryConfig, page.Contents)
+		s.RenderGroupContent(out, group, page.EntryConfig, page.Contents)
 	}
 }
 
-func (s SimplePageRender) RenderGroupContent(group page.Group, config page.EntryConfig, configContent page.ContentsConfig) {
-	s.b.WriteString("## ")
-	s.b.WriteString(group.Title)
-	s.b.WriteRune('\n')
-	s.b.WriteRune('\n')
+func (s SimplePageRender) RenderGroupContent(out io.StringWriter, group page.Group, config page.EntryConfig, configContent page.ContentsConfig) {
+	out.WriteString("## ")
+	out.WriteString(group.Title)
+	out.WriteString("\n")
+	out.WriteString("\n")
 
 	for _, q := range group.Entries {
-		s.RenderEntryContent(q, config, configContent)
+		s.RenderEntryContent(out, q, config, configContent)
 	}
 }
 
@@ -88,103 +74,103 @@ type PageLink struct {
 	URL  string
 }
 
-func (v PageLink) Render(b *strings.Builder) {
-	b.WriteString("[" + v.Name + "]")
-	b.WriteString("(" + v.URL + ")")
+func (v PageLink) RenderTo(out io.StringWriter) {
+	out.WriteString("[" + v.Name + "]")
+	out.WriteString("(" + v.URL + ")")
 }
 
-func (s SimplePageRender) RenderEntryContent(entry page.Entry, config page.EntryConfig, configContent page.ContentsConfig) {
+func (s SimplePageRender) RenderEntryContent(out io.StringWriter, entry page.Entry, config page.EntryConfig, configContent page.ContentsConfig) {
 	// title
-	s.b.WriteString("### ")
+	out.WriteString("### ")
 	if config.Back != "" {
 		url := "#" + strings.ReplaceAll(strings.ToLower(strings.TrimSpace(configContent.Title)), " ", "-")
 		v := PageLink{Name: config.Back, URL: url}
-		v.Render(s.b)
+		v.RenderTo(out)
 	}
-	s.b.WriteString(config.TitlePrefix)
-	s.b.WriteRune(' ')
-	s.b.WriteString(entry.Title)
-	s.b.WriteRune('\n')
-	s.b.WriteRune('\n')
+	out.WriteString(config.TitlePrefix)
+	out.WriteString(" ")
+	out.WriteString(entry.Title)
+	out.WriteString("\n")
+	out.WriteString("\n")
 
 	// description
-	s.b.WriteString(entry.Description)
+	out.WriteString(entry.Description)
 
 	// description: author
 	if len(entry.Author) > 0 {
-		s.b.WriteString(" — ")
-		s.b.WriteString(s.authorRenderer.Render(entry.Author))
+		out.WriteString(" — ")
+		out.WriteString(s.authorRenderer.Render(entry.Author))
 	}
 
 	// description: source
 	if len(entry.Source) > 0 {
-		s.b.WriteString(" / ")
-		s.b.WriteString(entry.Source)
+		out.WriteString(" / ")
+		out.WriteString(entry.Source)
 	}
 
 	// description: end
-	s.b.WriteRune('\n')
-	s.b.WriteRune('\n')
+	out.WriteString("\n")
+	out.WriteString("\n")
 
 	// commands
 	if len(entry.Commands) > 0 {
-		s.b.WriteRune('\n')
-		s.b.WriteString("```\n")
+		out.WriteString("\n")
+		out.WriteString("```\n")
 
 		for _, q := range entry.Commands {
-			s.b.WriteString(q)
-			s.b.WriteRune('\n')
+			out.WriteString(q)
+			out.WriteString("\n")
 		}
 
-		s.b.WriteString("```")
-		s.b.WriteRune('\n')
-		s.b.WriteRune('\n')
+		out.WriteString("```")
+		out.WriteString("\n")
+		out.WriteString("\n")
 	}
 
 	if entry.ExampleContent != "" {
-		s.b.WriteString("```")
+		out.WriteString("```")
 		if entry.ExampleContentExt != "" {
-			s.b.WriteString(entry.ExampleContentExt)
+			out.WriteString(entry.ExampleContentExt)
 		}
-		s.b.WriteRune('\n')
-		s.b.WriteString(entry.ExampleContent)
-		s.b.WriteString("```")
-		s.b.WriteRune('\n')
-		s.b.WriteRune('\n')
+		out.WriteString("\n")
+		out.WriteString(entry.ExampleContent)
+		out.WriteString("```")
+		out.WriteString("\n")
+		out.WriteString("\n")
 	}
 
 	if entry.ExampleOutput != "" {
-		s.b.WriteString(config.Example.Title)
-		s.b.WriteRune('\n')
-		s.b.WriteString("```\n")
-		s.b.WriteString(entry.ExampleOutput)
-		s.b.WriteString("```")
-		s.b.WriteRune('\n')
-		s.b.WriteRune('\n')
+		out.WriteString(config.Example.Title)
+		out.WriteString("\n")
+		out.WriteString("```\n")
+		out.WriteString(entry.ExampleOutput)
+		out.WriteString("```")
+		out.WriteString("\n")
+		out.WriteString("\n")
 	}
 
 	if entry.ExampleImageURL != "" {
-		s.b.WriteString(renderExampleImage(entry.ExampleImageURL))
-		s.b.WriteRune('\n')
-		s.b.WriteRune('\n')
+		out.WriteString(renderExampleImage(entry.ExampleImageURL))
+		out.WriteString("\n")
+		out.WriteString("\n")
 	}
 
 	// requirements
 	if len(entry.Requirements) > 0 {
-		s.b.WriteString(config.Requirements.Title)
-		s.b.WriteRune('\n')
-		s.b.WriteString("```\n")
+		out.WriteString(config.Requirements.Title)
+		out.WriteString("\n")
+		out.WriteString("```\n")
 
 		for _, q := range entry.Requirements {
-			s.b.WriteString(q)
-			s.b.WriteRune('\n')
+			out.WriteString(q)
+			out.WriteString("\n")
 		}
 
-		s.b.WriteString("```")
-		s.b.WriteRune('\n')
+		out.WriteString("```")
+		out.WriteString("\n")
 	}
 
-	s.b.WriteRune('\n')
+	out.WriteString("\n")
 }
 
 func renderExampleImage(imageURL string) string {
@@ -195,7 +181,7 @@ func renderExampleImage(imageURL string) string {
 	b.WriteString(imageURL)
 	b.WriteString(`" style="margin: 8px; max-height: 640px;">`)
 	b.WriteString(`</div>`)
-	b.WriteRune('\n')
+	b.WriteString("\n")
 	return b.String()
 }
 
