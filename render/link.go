d6ac0b7710429924c9537d7068de6108c4d9aca4
Nikolay <nikolay.dubina.pub@gmail.com>
2023-05-24 22:17:53 +0800

    nit

--- render/link.go
+++ render/link.go
@@ -1,6 +1,10 @@
 package render
 
-import "io"
+import (
+	"io"
+	"strings"
+	"unicode"
+)
 
 type Link struct {
 	Name string
@@ -10,4 +14,20 @@ type Link struct {
 func (v Link) RenderTo(out io.StringWriter) {
 	out.WriteString("[" + v.Name + "]")
 	out.WriteString("(" + v.URL + ")")
+}
+
+// makeMarkdownTitleLink produces name that can be used to reference Markdown section for entry
+func makeMarkdownTitleLink(s string) string {
+	t := strings.ReplaceAll(strings.ToLower(strings.TrimSpace(s)), " ", "-")
+
+	// keep only alpha numerics and space
+	var f strings.Builder
+	f.Grow(len(t))
+	for _, q := range t {
+		if unicode.IsLetter(q) || unicode.IsDigit(q) || q == '-' {
+			f.WriteRune(q)
+		}
+	}
+
+	return "#-" + f.String()
 }
--- render/simple_render.go
+++ render/simple_render.go
@@ -3,7 +3,6 @@ package render
 import (
 	"io"
 	"strings"
-	"unicode"
 
 	"github.com/nikolaydubina/mdpage/page"
 )
@@ -173,20 +172,4 @@ func (s SimplePageRender) RenderEntryContent(out io.StringWriter, entry page.Ent
 	}
 
 	out.WriteString("\n")
-}
-
-// makeMarkdownTitleLink produces name that can be used to reference Markdown section for entry
-func makeMarkdownTitleLink(s string) string {
-	t := strings.ReplaceAll(strings.ToLower(strings.TrimSpace(s)), " ", "-")
-
-	// keep only alpha numerics and space
-	var f strings.Builder
-	f.Grow(len(t))
-	for _, q := range t {
-		if unicode.IsLetter(q) || unicode.IsDigit(q) || q == '-' {
-			f.WriteRune(q)
-		}
-	}
-
-	return "#-" + f.String()
 }
