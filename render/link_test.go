e4c12541c04b9057a1cc3dbfc3fb9070630e9a5a
Nikolay <nikolay.dubina.pub@gmail.com>
2023-05-28 06:29:01 +0800

    nit

---
+++ render/link_test.go
@@ -0,0 +1,26 @@
+package render
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_makeMarkdownTitleLink(t *testing.T) {
+	tests := []struct {
+		v   string
+		exp string
+	}{
+		{
+			v:   "(archived) Make sure `if` statements using short assignment",
+			exp: "#-archived-make-sure-if-statements-using-short-assignment",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%v", tc), func(t *testing.T) {
+			got := makeMarkdownTitleLink(tc.v)
+			if got != tc.exp {
+				t.Error("got", got, "exp", tc.exp)
+			}
+		})
+	}
+}
